84b61c87e16a8d7878a85f9f59948c932aafa336
zakky <kycraft1164@gmail.com>
2024-12-03 01:46:30 +0700

    feat(auth service): create ArrayContains helper func

--- auth_service/utils/helper/helper.go
+++ auth_service/utils/helper/helper.go
@@ -7,3 +7,17 @@ import (
 func GenerateUUID() string {
 	return uuid.New().String()
 }
+
+func ArrayContains(arr interface{}, item interface{}) bool {
+	newArr, ok := arr.([]interface{})
+	if !ok {
+		return false
+	}
+
+	for _, v := range newArr {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
