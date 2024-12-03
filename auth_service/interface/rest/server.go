c40746f48ceec107b99b056184024948000b98fd
zakky <kycraft1164@gmail.com>
2024-12-03 21:22:03 +0700

    fix(auth service): missing handler registrartion and db migrations

--- auth_service/interface/rest/server.go
+++ auth_service/interface/rest/server.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"auth_service/config"
 	"auth_service/domain/dto"
+	"auth_service/domain/model"
 	"auth_service/interface/rest/handler"
 	"auth_service/middleware"
 	"auth_service/repository"
@@ -13,15 +14,27 @@ import (
 	_ "auth_service/docs"
 
 	"github.com/gin-gonic/gin"
+	"github.com/op/go-logging"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var logger = logging.MustGetLogger("rest")
 
 func SetupServer(router *gin.Engine) {
 	_ = context.Background()
 
 	responseWriter := http_response.NewHttpResponseWriter()
 	gormDB := config.NewPostgresqlDB()
+
+	// migrations
+	err := gormDB.AutoMigrate(
+		&model.User{},
+		&model.RefreshToken{},
+	)
+	if err != nil {
+		logger.Fatalf("failed to migrate database: %v", err)
+	}
 
 	// repositories
 	userRepo := repository.NewUserRepo(gormDB)
@@ -33,12 +46,18 @@ func SetupServer(router *gin.Engine) {
 	// handlers
 	authHandler := handler.NewAuthHandler(responseWriter, authUcase)
 	_ = authHandler
+
+	// register routes
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, dto.BaseJSONResp{
 			Code:    200,
 			Message: "pong",
 		})
 	})
+	router.POST("/auth/register", authHandler.Register)
+	router.POST("/auth/login", authHandler.Login)
+	router.POST("/auth/check-token", authHandler.CheckToken)
+	router.POST("/auth/refresh-token", authHandler.RefreshToken)
 
 	secureRouter := router.Group("/")
 	{
