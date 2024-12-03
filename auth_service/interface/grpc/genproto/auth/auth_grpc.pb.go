e90df9748651244ba0c126e75e883ba2bd1a37ba
zakky <kycraft1164@gmail.com>
2024-12-03 12:41:36 +0700

    ref(auth service): refactor grpc folder structure

--- auth_service/interface/grpc/genproto/auth/auth_grpc.pb.go
+++ auth_service/interface/grpc/genproto/auth/auth_grpc.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.0
-// source: auth_service.proto
+// source: auth.proto
 
-package genproto
+package auth
 
 import (
 	context "context"
@@ -117,5 +117,5 @@ var AuthService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "auth_service.proto",
+	Metadata: "auth.proto",
 }
