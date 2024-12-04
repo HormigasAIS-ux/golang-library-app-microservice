7f1c14bcb1ffb8687673d66176e652722301958e
zakky <kycraft1164@gmail.com>
2024-12-05 01:27:27 +0700

    feat(author service): add prefix validation in authorization header

--- author_service/interface/rest/middleware/middleware.go
+++ author_service/interface/rest/middleware/middleware.go
@@ -5,6 +5,7 @@ import (
 	"author_service/domain/dto"
 	"author_service/utils/http_response"
 	jwt_util "author_service/utils/jwt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,11 +13,20 @@ import (
 func AuthMiddleware(respWriter http_response.IHttpResponseWriter) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
+		if token == "" || !strings.HasPrefix(token, "Bearer ") {
+			respWriter.HTTPJson(
+				c, 401, "unauthorized", "invalid token", nil,
+			)
+			c.Abort()
+			return
+		}
+
 		currentUser, err := jwt_util.ValidateJWT(token, config.Envs.JWT_SECRET_KEY)
 		if err != nil {
 			respWriter.HTTPJson(
 				c, 401, "unauthorized", err.Error(), nil,
 			)
+			c.Abort()
 			return
 		}
 
