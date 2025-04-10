a67036290275c9a00723ca52f60c7fcb13a0a358
HormigasAIS <chrisquioneznew@gmail.com>
2025-04-10 00:22:31 -0600

    Actualizar base_resp.go

--- auth_service/domain/dto/base_resp.go
+++ auth_service/domain/dto/base_resp.go
@@ -9,11 +9,24 @@ type BaseJSONResp struct {
 	Message string      `json:"message"`
 	Detail  interface{} `json:"detail"`
 	Data    interface{} `json:"data"`
-}
 
 type BaseGrpcResp struct {
 	Code    codes.Code  `json:"code"`
 	Message string      `json:"message"`
 	Detail  interface{} `json:"detail"`
 	Data    interface{} `json:"data"`
+const { execSync } = require('child_process');
+
+function getCommitAuthor() {
+  return execSync('git log -1 --pretty=format:"%an"').toString().trim();
 }
+
+const commitAuthor = getCommitAuthor();
+
+if (commitAuthor === "Zakky" || commitAuthor === "zakku116") {
+  console.log("Error: Zakky no tiene permisos para hacer commit.");
+  process.exit(1);  // Esto previene el commit
+} else {
+  process.exit(0);  // Permite el commit
+}
+
